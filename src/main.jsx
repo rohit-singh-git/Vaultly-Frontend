d158ba3d29c1b3b15de787c57cf223cfe7c688e3
Rohit Singh <smartyonpc@gmail.com>
2026-08-25 12:11:39 +0530

    bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename='/Vaultly-Frontend'>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+    const decodedRedirect = decodeURIComponent(redirect);
+
+    window.history.replaceState(
+        null,
+        "",
+        "/Vaultly-Frontend" + decodedRedirect,
+    );
+}
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter basename="/Vaultly-Frontend">
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
+);
